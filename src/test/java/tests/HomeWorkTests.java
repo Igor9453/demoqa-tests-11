d9e0359edf082301df5089672b3a58ed4a27e99e
Igor <l.i.e@mail.ru>
2022-03-10 23:53:27 +0700

    Not don.txt

--- src/test/java/tests/HomeWorkTests.java
+++ src/test/java/tests/HomeWorkTests.java
@@ -1,8 +1,10 @@
-package Tests;
+package tests;
 
 import com.codeborne.selenide.Configuration;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
+
+import java.io.File;
 
 import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Selenide.$;
@@ -23,16 +25,19 @@ public class HomeWorkTests {
         $(".main-header").shouldHave(text("Practice-Form"));
 
         $("#firstName").setValue("ROK");
+        $("#lastName").setValue("ROK-Hard");
         $("#userEmail").setValue("ROK@comp.su");
         $("#genterWrapper").setValue("gender-radio-1");
         $("#userNumber").setValue("+7-999-450-55-65");
         $("#dateOfBirthInput").setValue("11.01.1991");
         $("#subjectsContainer").setValue("history");
         $("#subjects-label").setValue("hobbies-checkbox-3");
+        $("#uploadPicture").uploadFile(new File("src/test/resources/intro.jpg"));
         $("#currentAddress").setValue("Rok address 1");
         $("#submit").click();
 
-        $("#output").shouldHave(text("ROK"), text("ROK@comp.su"), text("Male"),
-                text("+7-999-450-55-65"), text("11.01.1991"), text("history"), text("music"), text("Rok address 1"));
+        $("#output").shouldHave(text("ROK"), text("ROK-Hard"), text("ROK@comp.su"), text("Male"),
+                text("+7-999-450-55-65"), text("11.01.1991"), text("history"), text("music"), text("intro.jpg"),
+                text("Rok address 1"));
     }
 }
